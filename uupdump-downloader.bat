23740b1d59580750a33830325a1be84e22f75163
Michael <70703680+michelle1574@users.noreply.github.com>
2023-05-14 21:58:25 +0300

    Add a slow internet option (UUP dump only)

--- uupdump-downloader.bat
+++ uupdump-downloader.bat
@@ -12,19 +12,32 @@ set /p "folder=To which folder the UUP files should be downloaded: "
 echo Selected ID: %id% >> %log%
 echo Selected language: %pack% >> %log%
 echo Selected editions: %index% >> %log%
+choice /c YN /M "Slow Internet? (Y/N)"
+if errorlevel==1 set "slowInter=1"
+if errorlevel==2 set "slowInter=0"
 choice /c YN /M "Download apps? (Y/N)"
 if errorlevel==2 goto downloaduups
 echo Please wait...
+if "slowInter==1" (
+aria2c -o %apps% "https://uupdump.net/findfiles.php?id=%id%&pack=neutral&edition=app&aria2=2" >> %log%
+aria2c -i %apps% -d %folder% >> %log%
+) else (
 aria2c -o %apps% "https://uupdump.net/get.php?id=%id%&pack=neutral&edition=app&aria2=2" >> %log%
 aria2c -i %apps% -d %folder% >> %log%
+)
 echo Apps were downloaded. >> %log%
 echo Finished.
 goto downloaduups
 
 :downloaduups
 echo Please wait...
-aria2c -o %uups% "https://uupdump.net/get.php?id=%id%&pack=%pack%&edition=%index%&aria2=2" >> %log%
-aria2c -i %uups% -d %folder% >> %log%
+if "slowInter==1" (
+aria2c -o %apps% "https://uupdump.net/findfiles.php?id=%id%&pack=%pack%&edition=%index%&aria2=2" >> %log%
+aria2c -i %apps% -d %folder% >> %log%
+) else (
+aria2c -o %apps% "https://uupdump.net/get.php?id=%id%&pack=%pack%&edition=%index%&aria2=2" >> %log%
+aria2c -i %apps% -d %folder% >> %log%
+)
 echo UUPs files finished downloading. >> %log%
 echo Finished.
 pause
